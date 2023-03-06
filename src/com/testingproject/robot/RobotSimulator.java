87cb5afbd99082ab913684df58bb48f2012fea1e
= <=>
2023-03-06 18:53:08 -0500

    Updated Junit test cases & exception Handling

--- src/com/testingproject/robot/RobotSimulator.java
+++ src/com/testingproject/robot/RobotSimulator.java
@@ -8,65 +8,65 @@ public class RobotSimulator {
 	static boolean penDown;
 	static int facing; // 0: north, 1: east, 2: south, 3: west
 	static final int MAX_SIZE = 100;
+	static int size;
 
 	public static void main(String[] args) {
 
-		try {
-			Scanner sc = new Scanner(System.in);
+		RobotSimulator.input();
+	}
 
-			System.out.print("Enter command: ");
-			String command = sc.nextLine();
+	public static void input() {
+ 
+		Scanner sc = new Scanner(System.in);
 
-			while (!command.equals("Q")) {
-				switch (command) {
-				case "I":
-					intializeFloor();
-					break;
-				case "C":
-					System.out.println(
-							"Position: " + x + ", " + y + " - Pen: " + (penDown ? "down" : "up") + " - Facing: "
-									+ (facing == 0 ? "north" : facing == 1 ? "east" : facing == 2 ? "south" : "west"));
-					break;
-				case "D":
-					penD();
-					break;
-				case "U":
-					penU();
-					break;
-				case "M":
-					move();
-					break;
-				case "R":
-					turnRight();
-					break;
-				case "L":
-					turnLeft();
-					break;
-				case "P":
-					printFloor();
-					break;
+		System.out.print("Enter command: ");
+		String command = sc.nextLine();
 
-				default:
-					System.out.println("Invalid command");
-				}
-				System.out.print("Enter command: ");
-				command = sc.nextLine();
+		while (!command.equals("Q")) {
+			switch (command) {
+			case "I":
+				intializeFloor();
+				break;
+			case "C": 
+				System.out.println("Position: " + x + ", " + y + " - Pen: " + (penDown ? "down" : "up") + " - Facing: "
+						+ (facing == 0 ? "north" : facing == 1 ? "east" : facing == 2 ? "south" : "west"));
+				break;
+			case "D":
+				penD();
+				break;
+			case "U":
+				penU();
+				break;
+			case "M":
+				move();
+				break;
+			case "R":
+				turnRight();
+				break;
+			case "L":
+				turnLeft();
+				break;
+			case "P":
+				printFloor();
+				break;
+
+			default:
+				System.out.println("Invalid command");
 			}
-
-		} catch (Exception e) {
-
-			System.out.print("Please provide valid input to move robot ");
+			System.out.print("Enter command: ");
+			command = sc.nextLine();
 		}
+
 	}
 
 	public static int[][] intializeFloor() {
 		Scanner sc = new Scanner(System.in);
 
-		int size = Integer.parseInt(sc.nextLine());
+		size = Integer.parseInt(sc.nextLine());
 
 		if (size <= 100) {
 
-			floor = new int[size][size];
+			floor = new int[size][size]; 
 			penDown = false;
 			facing = 0;
 		} else {
@@ -78,7 +78,7 @@ public class RobotSimulator {
 
 	}
 
-	private static void printFloor() {
+	public static void printFloor() {
 		System.out.println("Displaying the Floor:");
 		for (int i = floor[0].length - 1; i >= 0; i--) {
 
@@ -105,64 +105,95 @@ public class RobotSimulator {
 		return penDown = false;
 	}
 
-	static void moveNorth(int num) {
-		y += num;
+	static boolean moveNorth(int steps) {
+		int current = y + steps;
+		if (current >= size || current < 0) {
+			System.out.println("Invalid move");
+			return false;
+		}
+		y += steps;
+		return true;
+
 	}
 
-	static void moveEast(int num) {
-		x += num;
+	public static boolean moveEast(int steps) {
+		int current = x + steps;
+		if (current >= size || current < 0) {
+			System.out.println("Invalid move");
+			return false;
+		}
+		x += steps;
+		return true;
+
 	}
-	
-	static void moveSouth(int num) {
-		y -= num;
+
+	public static boolean moveSouth(int steps) {
+		int current = y - steps;
+		if (current >= size || current < 0) {
+			System.out.println("Invalid move");
+			return false;
+		}
+		y = y - steps;
+		return true;
+
 	}
-	
-	static void moveWest(int num) {
-		x -= num;
+
+	public static boolean moveWest(int steps) {
+		int current = x - steps;
+		if (current >= size || current < 0) {
+			System.out.println("Invalid move");
+			return false;
+		}
+		x -= steps;
+		return true;
+
 	}
 
 	public static void move() {
-		try {
-			Scanner sc = new Scanner(System.in);
-			int steps = Integer.parseInt(sc.nextLine());
+		Scanner sc = new Scanner(System.in);
+		int steps = Integer.parseInt(sc.nextLine());
 
-			switch (facing) {
-			case 0:
-				moveNorth(steps);
-				break;
-			case 1:
-				moveEast(steps);
-				break;
-			case 2:
-				moveSouth(steps);
-				break;
-			case 3:
-				moveWest(steps);
-				break;
-			}
-			if (penDown) {
-				for (int i = 0; i < steps; i++) {
-					switch (facing) {
-					case 0:
-						floor[y - i][x] = 1;
-						break;
-					case 1:
-						floor[y][x - i] = 1;
-						break;
-					case 2:
-						floor[y + i][x] = 1;
-						break;
-					case 3:
-						floor[y][x + i] = 1;
-						break;
-					}
+		boolean status = true;
+		switch (facing) {
+		case 0:
+			status = moveNorth(steps);
+			break;
+		case 1: 
+			status = moveEast(steps);
+			break; 
+		case 2:
+			status = moveSouth(steps);
+			break;
+		case 3:
+			status = moveWest(steps);
+			break;
+		}
+		if (!status)
+			return;
+		if (penDown) {
+			for (int i = 0; i < steps; i++) {
+				switch (facing) { 
+				case 0:
+					floor[y - i][x] = 1;
+					break;
+				case 1: 
+					floor[y][x - i] = 1;
+					break;
+				case 2:
+
+					floor[y + i][x] = 1;
+
+					break;
+
+				case 3:
+
+					floor[y][x + i] = 1;
+
+					break;
 				}
 			}
-
-		} catch (Exception e) {
-
-			System.out.println("INVALID MOVE : ROBOT IS OUTSIDE OF THE FLOOR");
 		}
+
 	}
 }
 
